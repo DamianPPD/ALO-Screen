51bb663bf48cfae5138dd5d8965556116e9602a2
DamianPPD <DK_rozne@proton.me>
2026-09-14 19:13:50 +0200

    build: add Windows build script

---
+++ build.bat
@@ -0,0 +1,10 @@
+@echo off
+setlocal
+set CGO_ENABLED=0
+set GOOS=windows
+set GOARCH=amd64
+go test ./...
+if errorlevel 1 exit /b 1
+go build -trimpath -ldflags="-H=windowsgui -s -w" -o ALO-Screen-0.1.exe ./cmd/aloscreen
+if errorlevel 1 exit /b 1
+echo Gotowe: ALO-Screen-0.1.exe
