220b24dc0d259b92aea69fbc2f310dfde2f22697
DamianPPD <DK_rozne@proton.me>
2026-09-14 19:15:29 +0200

    feat: add Windows API bindings

---
+++ cmd/aloscreen/winapi_windows.go
@@ -0,0 +1,226 @@
+//go:build windows
+
+package main
+
+import (
+	"syscall"
+	"unsafe"
+)
+
+var (
+	user32   = syscall.NewLazyDLL("user32.dll")
+	gdi32    = syscall.NewLazyDLL("gdi32.dll")
+	shell32  = syscall.NewLazyDLL("shell32.dll")
+	kernel32 = syscall.NewLazyDLL("kernel32.dll")
+
+	procRegisterClassExW              = user32.NewProc("RegisterClassExW")
+	procCreateWindowExW               = user32.NewProc("CreateWindowExW")
+	procDefWindowProcW                = user32.NewProc("DefWindowProcW")
+	procDestroyWindow                 = user32.NewProc("DestroyWindow")
+	procGetMessageW                   = user32.NewProc("GetMessageW")
+	procTranslateMessage              = user32.NewProc("TranslateMessage")
+	procDispatchMessageW              = user32.NewProc("DispatchMessageW")
+	procPostQuitMessage               = user32.NewProc("PostQuitMessage")
+	procRegisterHotKey                = user32.NewProc("RegisterHotKey")
+	procUnregisterHotKey              = user32.NewProc("UnregisterHotKey")
+	procLoadIconW                     = user32.NewProc("LoadIconW")
+	procLoadCursorW                   = user32.NewProc("LoadCursorW")
+	procMessageBoxW                   = user32.NewProc("MessageBoxW")
+	procCreatePopupMenu               = user32.NewProc("CreatePopupMenu")
+	procAppendMenuW                   = user32.NewProc("AppendMenuW")
+	procTrackPopupMenu                = user32.NewProc("TrackPopupMenu")
+	procDestroyMenu                   = user32.NewProc("DestroyMenu")
+	procGetCursorPos                  = user32.NewProc("GetCursorPos")
+	procSetForegroundWindow           = user32.NewProc("SetForegroundWindow")
+	procSetProcessDpiAwarenessContext = user32.NewProc("SetProcessDpiAwarenessContext")
+	procEnumDisplayMonitors           = user32.NewProc("EnumDisplayMonitors")
+	procGetMonitorInfoW               = user32.NewProc("GetMonitorInfoW")
+	procGetDC                         = user32.NewProc("GetDC")
+	procReleaseDC                     = user32.NewProc("ReleaseDC")
+	procOpenClipboard                 = user32.NewProc("OpenClipboard")
+	procCloseClipboard                = user32.NewProc("CloseClipboard")
+	procEmptyClipboard                = user32.NewProc("EmptyClipboard")
+	procSetClipboardData              = user32.NewProc("SetClipboardData")
+	procEnumWindows                   = user32.NewProc("EnumWindows")
+	procIsWindowVisible               = user32.NewProc("IsWindowVisible")
+	procGetWindowTextLengthW          = user32.NewProc("GetWindowTextLengthW")
+	procGetWindowTextW                = user32.NewProc("GetWindowTextW")
+	procShowWindow                    = user32.NewProc("ShowWindow")
+	procBringWindowToTop              = user32.NewProc("BringWindowToTop")
+	procKeybdEvent                    = user32.NewProc("keybd_event")
+
+	procCreateCompatibleDC     = gdi32.NewProc("CreateCompatibleDC")
+	procDeleteDC               = gdi32.NewProc("DeleteDC")
+	procCreateCompatibleBitmap = gdi32.NewProc("CreateCompatibleBitmap")
+	procSelectObject           = gdi32.NewProc("SelectObject")
+	procDeleteObject           = gdi32.NewProc("DeleteObject")
+	procBitBlt                 = gdi32.NewProc("BitBlt")
+	procGetDIBits              = gdi32.NewProc("GetDIBits")
+
+	procShellNotifyIconW = shell32.NewProc("Shell_NotifyIconW")
+
+	procGetModuleHandleW = kernel32.NewProc("GetModuleHandleW")
+	procGlobalAlloc      = kernel32.NewProc("GlobalAlloc")
+	procGlobalLock       = kernel32.NewProc("GlobalLock")
+	procGlobalUnlock     = kernel32.NewProc("GlobalUnlock")
+	procGlobalFree       = kernel32.NewProc("GlobalFree")
+	procRtlMoveMemory    = kernel32.NewProc("RtlMoveMemory")
+)
+
+const (
+	wmDestroy   = 0x0002
+	wmCommand   = 0x0111
+	wmHotKey    = 0x0312
+	wmLButtonUp = 0x0202
+	wmRButtonUp = 0x0205
+	wmApp       = 0x8000
+	wmTrayIcon  = wmApp + 1
+
+	vkF8      = 0x77
+	vkControl = 0x11
+	vkV       = 0x56
+
+	modNoRepeat = 0x4000
+
+	nimAdd    = 0x00000000
+	nimDelete = 0x00000002
+	nimSetVer = 0x00000004
+
+	nifMessage = 0x00000001
+	nifIcon    = 0x00000002
+	nifTip     = 0x00000004
+
+	notifyIconVersion4 = 4
+
+	idiApplication = 32512
+	idcArrow       = 32512
+
+	mfString = 0x00000000
+
+	tpmRightButton = 0x0002
+	tpmBottomAlign = 0x0020
+	tpmLeftAlign   = 0x0000
+
+	mbIconError = 0x00000010
+	mbOK        = 0x00000000
+
+	swRestore = 9
+
+	keyeventfKeyup = 0x0002
+
+	monitorinfofPrimary = 0x00000001
+
+	srccopy    = 0x00CC0020
+	captureblt = 0x40000000
+
+	dibRGBColors = 0
+	biRGB        = 0
+
+	cfDIB = 8
+
+	gmemMoveable = 0x0002
+)
+
+type point struct {
+	X int32
+	Y int32
+}
+
+type rect struct {
+	Left   int32
+	Top    int32
+	Right  int32
+	Bottom int32
+}
+
+type msg struct {
+	HWnd     uintptr
+	Message  uint32
+	WParam   uintptr
+	LParam   uintptr
+	Time     uint32
+	Pt       point
+	LPrivate uint32
+}
+
+type wndClassEx struct {
+	CbSize        uint32
+	Style         uint32
+	LpfnWndProc   uintptr
+	CbClsExtra    int32
+	CbWndExtra    int32
+	HInstance     uintptr
+	HIcon         uintptr
+	HCursor       uintptr
+	HbrBackground uintptr
+	LpszMenuName  *uint16
+	LpszClassName *uint16
+	HIconSm       uintptr
+}
+
+type notifyIconData struct {
+	CbSize           uint32
+	HWnd             uintptr
+	UID              uint32
+	UFlags           uint32
+	UCallbackMessage uint32
+	HIcon            uintptr
+	SzTip            [128]uint16
+	DwState          uint32
+	DwStateMask      uint32
+	SzInfo           [256]uint16
+	UVersionTimeout  uint32
+	SzInfoTitle      [64]uint16
+	DwInfoFlags      uint32
+	GuidItem         [16]byte
+	HBalloonIcon     uintptr
+}
+
+type monitorInfoEx struct {
+	CbSize    uint32
+	RcMonitor rect
+	RcWork    rect
+	DwFlags   uint32
+	SzDevice  [32]uint16
+}
+
+type bitmapInfoHeader struct {
+	BiSize          uint32
+	BiWidth         int32
+	BiHeight        int32
+	BiPlanes        uint16
+	BiBitCount      uint16
+	BiCompression   uint32
+	BiSizeImage     uint32
+	BiXPelsPerMeter int32
+	BiYPelsPerMeter int32
+	BiClrUsed       uint32
+	BiClrImportant  uint32
+}
+
+type bitmapInfo struct {
+	Header bitmapInfoHeader
+	Colors [1]uint32
+}
+
+func utf16Ptr(s string) *uint16 {
+	p, _ := syscall.UTF16PtrFromString(s)
+	return p
+}
+
+func copyUTF16(dst []uint16, s string) {
+	u := syscall.StringToUTF16(s)
+	if len(u) > len(dst) {
+		u = u[:len(dst)]
+		if len(u) > 0 {
+			u[len(u)-1] = 0
+		}
+	}
+	copy(dst, u)
+}
+
+func loword(v uintptr) uint16 { return uint16(v & 0xffff) }
+
+func showError(title, text string) {
+	procMessageBoxW.Call(0, uintptr(unsafe.Pointer(utf16Ptr(text))), uintptr(unsafe.Pointer(utf16Ptr(title))), mbOK|mbIconError)
+}
