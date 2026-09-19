e4c2af45ab3e128a29c1ebaf1016b5c5146d4c03
DamianPPD <DK_rozne@proton.me>
2026-09-19 23:56:08 +0200

    build: bump ALO Screen to 0.2

--- build.bat
+++ build.bat
@@ -5,6 +5,6 @@ set GOOS=windows
 set GOARCH=amd64
 go test ./...
 if errorlevel 1 exit /b 1
-go build -trimpath -ldflags="-H=windowsgui -s -w" -o ALO-Screen-0.1.exe ./cmd/aloscreen
+go build -trimpath -ldflags="-H=windowsgui -s -w" -o ALO-Screen-0.2.exe ./cmd/aloscreen
 if errorlevel 1 exit /b 1
-echo Gotowe: ALO-Screen-0.1.exe
+echo Gotowe: ALO-Screen-0.2.exe
